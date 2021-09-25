b6d700722f3438cc0cb888ea1176d58bb9bff94b
srujan <srujan001ssa@gmail.com>
2021-09-25 22:29:51 +0530

    refactor

--- quiz.js
+++ quiz.js
@@ -3,6 +3,8 @@ const submitAnswerBtn = document.querySelector("#submit-answer-btn")
 const outputElement = document.querySelectorAll(".output");
 const correctAnswer = ["Pythagoras theorem", "90°", "8","isoceles","Heron"];
 
+
+
 function calculateScore(){
   let score= 0;
   let index = 0;
